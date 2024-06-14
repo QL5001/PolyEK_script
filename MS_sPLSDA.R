afc78c64074b04ae3498ba9fc48bcaed32804c55
QL5001 <45108650+QL5001@users.noreply.github.com>
2024-06-14 10:06:10 -0500

    Update MS_sPLSDA.R

--- MS_sPLSDA.R
+++ MS_sPLSDA.R
@@ -1,1 +1,6 @@
+#Sparse partial least squares discriminant analysis (sPLSDA) on untargeted LC-MS profiling of Enterococcus faecalis metabolome of the whole genome sequences of 29 E. coli isolates including 13 high and 16 low biofilm formers
+#Beign#
 
+
+#Exported csv files will be used for further data analyses and drawing figures in Excel, Prism, and R programming
+#End#
